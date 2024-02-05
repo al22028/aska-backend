7888713a45f2d06b69d6734dd10727753b1dabec
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-02-06 01:25:42 +0900

    :gear: enabled swagger ui

--- src/v1/handlers/app.py
+++ src/v1/handlers/app.py
@@ -7,14 +7,8 @@ from pydantic import BaseModel
 tracer = Tracer()
 logger = Logger()
 
-app = APIGatewayRestResolver()
+app = APIGatewayRestResolver(enable_validation=True)
 app.enable_swagger(path="/swagger", title="Aska API")
-
-
-@tracer.capture_lambda_handler
-@logger.inject_lambda_context(log_event=True)
-def lambda_handler(event: dict, context: LambdaContext) -> dict[str, str | int]:
-    return app.resolve(event, context)
 
 
 class HealthCheckSchema(BaseModel):
@@ -22,5 +16,11 @@ class HealthCheckSchema(BaseModel):
 
 
 @app.get("/healthcheck")
-def health_check(event: dict, context: LambdaContext) -> HealthCheckSchema:
+def health_check() -> HealthCheckSchema:
     return HealthCheckSchema(**{"status": "ok"})
+
+
+# @tracer.capture_lambda_handler
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event: dict, context: LambdaContext) -> dict[str, str | int]:
+    return app.resolve(event, context)
