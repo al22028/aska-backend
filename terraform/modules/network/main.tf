437f5e4e53a7855342b23e124470a08116a07111
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-02-06 14:21:49 +0900

    :green_heart: add allow https security group

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -170,3 +170,27 @@ resource "aws_security_group" "allow_postgres" {
     Name = "allow-postgres-${var.suffix}"
   }
 }
+
+# Security Group for HTTPS
+resource "aws_security_group" "allow_https" {
+  name        = "allow-https"
+  description = "Allow https"
+  vpc_id      = aws_vpc.this.id
+  ingress {
+    description = "Https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow-https-${var.suffix}"
+  }
+}
--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -3,5 +3,5 @@ output "private_subnet_ids" {
 }
 
 output "security_group_ids" {
-  value = [aws_security_group.allow_postgres.id]
+  value = [aws_security_group.allow_postgres.id, aws_security_group.allow_https.id]
 }
