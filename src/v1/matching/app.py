3d21daef4e0f64d4cc49f6189ee034c589c41799
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-02-07 11:15:35 +0900

    :+1: add matching score calc

---
+++ src/v1/matching/app.py
@@ -0,0 +1,31 @@
+#  type: ignore
+
+import cv2
+
+
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+from aws_lambda_powertools.utilities.data_classes import event_source, LambdaFunctionUrlEvent
+
+logger = Logger(service="MatchingScoreCalculator")
+
+
+def score_similarity(desc_0, desc_1) -> float:
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    matches = bf.match(desc_0, desc_1)
+    dist = [m.distance for m in matches]
+    ret = sum(dist) / len(dist)
+    return ret
+
+
+@event_source(data_class=LambdaFunctionUrlEvent)
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event: LambdaFunctionUrlEvent, context: LambdaContext) -> dict:
+    body = event.body
+    print(body)
+
+    return {
+        "statusCode": 200,
+        "body": "OK",
+    }
