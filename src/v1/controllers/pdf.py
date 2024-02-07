9ea4bb174d99d1a473c0c389f84610c91a4dbc0d
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-02-07 18:34:27 +0900

    :sparkles: add pdf controller

---
+++ src/v1/controllers/pdf.py
@@ -0,0 +1,26 @@
+# Standard Library
+from http import HTTPStatus
+from typing import List
+
+# Third Party Library
+from database.base import Pdf
+
+from database.session import with_session
+from models.pdf import PdfORM
+from schemas import PdfCreateSchema, PdfSchema
+from sqlalchemy.orm.session import Session
+
+
+class ProjectController:
+
+    pdfs = PdfORM()
+
+    @with_session
+    def fetch_all_pdfs(self, session: Session) -> list[PdfSchema]:
+        pdfs: List[Pdf] = self.pdfs.find_all(db=session)
+        return [PdfSchema(**pdf.serializer()) for pdf in pdfs]
+
+    @with_session
+    def create_one(self, session: Session, pdf_data: PdfCreateSchema) -> tuple[PdfSchema, int]:
+        pdf = self.pdfs.create_one(db=session, pdf_data=pdf_data)
+        return PdfSchema(**pdf.serializer()), HTTPStatus.CREATED.value
