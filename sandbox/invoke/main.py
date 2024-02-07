4dcf8edc429a788e7eeba236d4fcc10c1919d52e
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-02-07 13:53:55 +0900

    :+1: fixing matching score protocol

--- sandbox/invoke/main.py
+++ sandbox/invoke/main.py
@@ -1,17 +1,33 @@
 # mypy: ignore-errors
 
 import boto3
+import json
 
 
 client = boto3.client("lambda")
 
+s3 = boto3.client("s3")
+
+
+def get_object_body(bucket: str, key: str) -> bytes:
+    response = s3.get_object(Bucket=bucket, Key=key)
+    return response["Body"].read().decode("utf-8")
+
 
 def invoke_lambda():
+    payload = {
+        "body": {
+            "bucket_name": "aska-image-bucket-dev",
+            "before": "id231321/1.json",
+            "after": "id231321/2.json",
+        }
+    }
+
     response = client.invoke(
-        FunctionName="aska-api-dev-MatchingCalculatHandler",
+        FunctionName="aska-api-dev-MatchingCalculateHandler",
         InvocationType="RequestResponse",
         LogType="Tail",
-        Payload=b'{"body": "hello"}',
+        Payload=bytes(json.dumps(payload).encode()),
     )
     print(response["Payload"].read().decode("utf-8"))
 
--- src/v1/matching_calculator/app.py
+++ src/v1/matching_calculator/app.py
@@ -1,12 +1,21 @@
 # type: ignore
+# Standard Library
+import json
 
 # Third Party Library
+import boto3
 import cv2
+import numpy as np
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.data_classes import LambdaFunctionUrlEvent, event_source
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 logger = Logger(service="MatchingScoreCalculator")
+
+client = boto3.client("s3")
+
+
+ACC = 0.2
 
 
 def score_similarity(desc_0, desc_1) -> float:
@@ -17,13 +26,24 @@ def score_similarity(desc_0, desc_1) -> float:
     return ret
 
 
+def get_object_body(bucket: str, key: str) -> str:
+    response = client.get_object(Bucket=bucket, Key=key)
+    return response["Body"].read().decode("utf-8")
+
+
 @event_source(data_class=LambdaFunctionUrlEvent)
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event: LambdaFunctionUrlEvent, context: LambdaContext) -> dict:
-    body = event.body
-    print(body)
-
-    return {
-        "statusCode": 200,
-        "body": "OK",
-    }
+    bucket_name = event.body["bucket_name"]
+    before_object_key, after_object_key = event.body["before"], event.body["after"]
+    before_data = get_object_body(bucket_name, before_object_key)
+    after_data = get_object_body(bucket_name, after_object_key)
+    after_descripter = json.loads(after_data)["descriptors"]
+    before_descriptor = json.loads(before_data)["descriptors"]
+    desc_num_ave = int(len(after_descripter) + len(before_descriptor)) / 2
+    desc_num = int(desc_num_ave * ACC)
+    score = score_similarity(
+        np.array(after_descripter[:desc_num], dtype=np.uint8),
+        np.array(before_descriptor[:desc_num], dtype=np.uint8),
+    )
+    return {"statusCode": 200, "score": score}
