90c6314d1e30db8f2783cefb40ba4bcfad6c7257
hasegawa-h-ww <hasegawa.h@world-wing.com>
2024-02-14 12:14:50 +0900

    add image controller

--- src/v1/config/settings.py
+++ src/v1/config/settings.py
@@ -22,6 +22,7 @@ SQLALCHEMY_ECHO_SQL = config("SQLALCHEMY_ECHO_SQL", default=False, cast=bool)
 
 # AWS S3
 AWS_PDF_BUCKET = config("AWS_PDF_BUCKET", default="aska-pdf-bucket-dev", cast=str)
+AWS_IMAGE_BACKET = config("AWS_IMAGE_BACKET", default="aska-image-bucket-dev", cast=str)
 
 # AWS CloudFront
 AWS_IMAGE_HOST_DOMAIN = config(
