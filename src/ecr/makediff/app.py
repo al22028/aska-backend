c4d96b04049cd716e6b98b4d5654d3981f25bd6e
zaki-t <zaki.t@world-wing.com>
2024-02-15 16:09:36 +0900

    catch ValidationError

--- src/ecr/makediff/app.py
+++ src/ecr/makediff/app.py
@@ -8,7 +8,7 @@ from aws_lambda_powertools.utilities.data_classes import LambdaFunctionUrlEvent,
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from calculator import Calculator
 from image import ImageModel, JsonModel
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
 logger = Logger(service="ImageDiffCalculator")
 
@@ -35,13 +35,16 @@ class EventBody(BaseModel):
 @event_source(data_class=LambdaFunctionUrlEvent)
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event: LambdaFunctionUrlEvent, context: LambdaContext) -> dict:
-    if isinstance(event.body, str):
-        event_body_dict = json.loads(event.body)
-    else:
-        event_body_dict = event.body
+    try:
+        if isinstance(event.body, str):
+            event_body_dict = json.loads(event.body)
+        else:
+            event_body_dict = event.body
 
-    event_body = EventBody(**event_body_dict)
-
+        event_body = EventBody(**event_body_dict)
+    except ValidationError as e:
+        logger(e)
+        raise e
     bucket_name = event_body.bucket_name
     before_json_object_key, after_json_object_key = (
         event_body.before.json_object_key,
