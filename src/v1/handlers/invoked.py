ca976568260fd71ac47bca5d4934881529a9ea28
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-06 12:14:53 +0900

    :bug: fixing parse event

--- src/v1/handlers/invoked.py
+++ src/v1/handlers/invoked.py
@@ -1,13 +1,14 @@
 # Standard Library
 
-from aws_lambda_powertools.utilities.parser import parse, ValidationError
-
-from schemas import LmabdaInvokePayloadSchema
+# Standard Library
+import json
 
 # Third Party Library
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.utilities.data_classes import LambdaFunctionUrlEvent, event_source
+from aws_lambda_powertools.utilities.parser import ValidationError, parse
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from schemas import LmabdaInvokePayloadSchema
 
 tracer = Tracer()
 logger = Logger()
@@ -18,7 +19,7 @@ logger = Logger()
 @event_source(data_class=LambdaFunctionUrlEvent)
 def lambda_handler(event: LambdaFunctionUrlEvent, context: LambdaContext) -> dict:
     try:
-        event = parse(event, LmabdaInvokePayloadSchema)
+        event = parse(json.loads(event.body), LmabdaInvokePayloadSchema)  # type: ignore
     except ValidationError as e:
         return {"statusCode": 400, "body": e.errors()}
 
