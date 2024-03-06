e9102d92a11d3cd94cbab0a9202cd4a877fd2ff2
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-06 11:04:46 +0900

    :bug: added lambda context type annotations

--- src/v1/handlers/invoked.py
+++ src/v1/handlers/invoked.py
@@ -3,13 +3,14 @@
 # Third Party Library
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.utilities.data_classes import LambdaFunctionUrlEvent, event_source
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 tracer = Tracer()
 logger = Logger()
 
 
-@event_source(data_class=LambdaFunctionUrlEvent)
-@logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler
-def lambda_handler(event: LambdaFunctionUrlEvent, context) -> dict[str, str | int]:
+@logger.inject_lambda_context(log_event=True)
+@event_source(data_class=LambdaFunctionUrlEvent)
+def lambda_handler(event: LambdaFunctionUrlEvent, context: LambdaContext) -> dict:
     return {"statusCode": 200, "body": event.body}
