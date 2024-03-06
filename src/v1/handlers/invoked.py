8e334c859ea60a256baadedba1cbfe3d6e785b1a
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-06 12:08:28 +0900

    :+1: add lambda invoked payload schema #200

--- src/v1/handlers/invoked.py
+++ src/v1/handlers/invoked.py
@@ -1,5 +1,9 @@
 # Standard Library
 
+from aws_lambda_powertools.utilities.parser import parse, ValidationError
+
+from schemas import LmabdaInvokePayloadSchema
+
 # Third Party Library
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.utilities.data_classes import LambdaFunctionUrlEvent, event_source
@@ -13,5 +17,9 @@ logger = Logger()
 @logger.inject_lambda_context(log_event=True)
 @event_source(data_class=LambdaFunctionUrlEvent)
 def lambda_handler(event: LambdaFunctionUrlEvent, context: LambdaContext) -> dict:
+    try:
+        event = parse(event, LmabdaInvokePayloadSchema)
+    except ValidationError as e:
+        return {"statusCode": 400, "body": e.errors()}
 
     return {"statusCode": 200, "body": event.body}
