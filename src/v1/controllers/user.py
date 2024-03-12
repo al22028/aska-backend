2bc7758e91d938301d4c05b67b65be49f19e9596
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-12 11:29:31 +0900

    :sparkles: add create cognito user controller #237 #238

--- src/v1/controllers/user.py
+++ src/v1/controllers/user.py
@@ -1,17 +1,42 @@
 # Standard Library
+import random
+import string
 from http import HTTPStatus
 from typing import List
 
 # Third Party Library
+from aws.cognito import Cognito
 from aws_lambda_powertools.event_handler.exceptions import BadRequestError, NotFoundError
 from database.base import User
 from database.session import with_session
 from models.user import UserORM
-from schemas.user import UserCreateSchema, UserSchema, UserUpdateSchema
+from mypy_boto3_cognito_idp.type_defs import AdminCreateUserResponseTypeDef
+from schemas.user import UserCreateResponsSchema, UserCreateSchema, UserSchema, UserUpdateSchema
 from sqlalchemy.orm.session import Session
 
 
+def generate_password() -> str:
+    """Generate random password
+
+    Returns:
+        str: random password
+    """
+    length = random.randint(8, 12)
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()"
+    # 最低1つの数字を確実に含むようにする
+    password = "".join(random.choice(characters) for i in range(length - 1)) + random.choice(
+        string.digits
+    )
+
+    password_list = list(password)
+    random.shuffle(password_list)
+    password = "".join(password_list)
+
+    return password
+
+
 class UserController:
+    cognito = Cognito()
 
     users = UserORM()
 
@@ -21,13 +46,31 @@ class UserController:
         return [UserSchema(**user.serializer()) for user in users]
 
     @with_session
-    def create_one(self, session: Session, user_data: UserCreateSchema) -> tuple[UserSchema, int]:
-        if self.users.exists(db=session, user_id=user_data.id):
-            raise BadRequestError("userId already in use")
+    def create_one(
+        self, session: Session, user_data: UserCreateSchema
+    ) -> tuple[UserCreateResponsSchema, int]:
         if self.users.find_by_email(db=session, email=user_data.email):
             raise BadRequestError("email already in use")
-        user = self.users.create_one(db=session, user_data=user_data)
-        return UserSchema(**user.serializer()), HTTPStatus.CREATED.value
+        # create user in cognito
+        password = generate_password()
+        _response: AdminCreateUserResponseTypeDef = self.cognito.create_user(
+            user_data.email, password
+        )
+        user_id = None
+        attributes = _response["User"]["Attributes"]
+        for attribute in attributes:
+            if attribute["Name"] == "sub":
+                user_id = attribute["Value"]
+                break
+        if not user_id:
+            raise BadRequestError("user id not found")
+        self.cognito.confirm_user(user_data.email, password)
+        self.cognito.verify_email(user_data.email)
+        # create user in database
+        user = self.users.create_one(db=session, id=user_id, user_data=user_data)
+        serialized_user = user.serializer()
+        serialized_user["password"] = password
+        return UserCreateResponsSchema(**serialized_user), HTTPStatus.CREATED.value
 
     @with_session
     def find_one(self, session: Session, user_id: str) -> UserSchema:
