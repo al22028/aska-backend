025f421d1a80f71231b63f29594b92b404f87d0b
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-12 11:26:15 +0900

    :books: fixing field title

--- src/v1/schemas/common.py
+++ src/v1/schemas/common.py
@@ -6,8 +6,8 @@ from schemas.base import BaseSchema
 class DeletedSchema(BaseSchema):
     message: str = Field(
         default="Object deleted successfully",
-        title="Message",
-        description="Message",
+        title="メッセージ",
+        description="削除成功",
         example="Object deleted successfully",  # type: ignore
     )
 
@@ -15,11 +15,13 @@ class DeletedSchema(BaseSchema):
 class TimeStampSchema(BaseSchema):
     created_at: str = Field(
         ...,
+        title="作成時刻",
         description="isoformated created datetime",
         example="2021-09-01T00:00:00.000Z",  # type: ignore
     )
     updated_at: str = Field(
         ...,
+        title="更新時刻",
         description="isoformated updated datetime",
         example="2021-09-01T00:00:00.000Z",  # type: ignore
     )
@@ -28,7 +30,7 @@ class TimeStampSchema(BaseSchema):
 class DownloadURLSchema(BaseSchema):
     presigned_url: str = Field(
         ...,
-        title="Download URL",
-        description="Download URL",
+        title="ダウンロードURL",
+        description="presigned URL for download file",
         example="https://example.com/download_url",  # type: ignore
     )
