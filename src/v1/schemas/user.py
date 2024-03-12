6d6e9f59b32bcc5a0ad92c57f38b5a788601b5ce
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-12 11:27:19 +0900

    :+1: add user create response schema #236

--- src/v1/schemas/user.py
+++ src/v1/schemas/user.py
@@ -5,27 +5,20 @@ from schemas.common import TimeStampSchema
 
 
 class UserCreateSchema(BaseSchema):
-    id: str = Field(
-        ...,
-        min_length=1,
-        max_length=256,
-        title="ID",
-        description="Same as Cognito sub",
-        example="44f97c86-d495-4afc-bdc6-f2443a159c28",  # type: ignore
-    )
     name: str = Field(
         ...,
         min_length=1,
         max_length=256,
-        title="Name",
-        description="Name",
+        title="名前",
+        description="名前",
         example="Tsubasa Taro",  # type: ignore
     )
     email: str = Field(
         ...,
         min_length=1,
         max_length=256,
-        description="Email",
+        title="メールアドレス",
+        description="メールアドレス",
         example="tsubasa@world-wing.com",  # type: ignore
     )
     model_config = ConfigDict(from_attributes=True)
@@ -36,11 +29,17 @@ class UserUpdateSchema(BaseSchema):
         ...,
         min_length=1,
         max_length=256,
-        title="Name",
-        description="Name",
+        title="名前",
+        description="名前",
         example="Tsubasa Taro",  # type: ignore
     )
 
 
 class UserSchema(UserCreateSchema, TimeStampSchema):
     pass
+
+
+class UserCreateResponsSchema(UserSchema):
+    password: str = Field(
+        ..., title="パスワード", description="初期パスワード", example="p@ssw0rd"  # type: ignore
+    )
