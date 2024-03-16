02a51a0d65fef480c27b1fdc4876c578b3d2b77c
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-17 06:00:49 +0900

    WIP: adding diff create

--- src/v1/controllers/diff.py
+++ src/v1/controllers/diff.py
@@ -23,3 +23,10 @@ class DiffController:
     def find_all(self, session: Session) -> list[DiffSchema]:
         diffs = self.matchings.find_all(session)
         return [DiffSchema(**diff.serializer()) for diff in diffs]
+
+    @with_session
+    def find_by_ids(self, image1_id: str, image2_id, session: Session) -> DiffSchema:
+        diff = self.matchings.find_by_ids(image1_id=image1_id, image2_id=image2_id, db=session)
+        return DiffSchema(**diff.serializer())
+
+    # @with_session
--- src/v1/models/matching.py
+++ src/v1/models/matching.py
@@ -6,6 +6,7 @@ from aws_lambda_powertools import Logger
 from database.base import Matching
 from sqlalchemy.orm.session import Session
 from views.console import log_function_execution
+from schemas.diff import DiffCreateSchema
 
 logger = Logger("MatchinORM")
 
@@ -15,3 +16,25 @@ class MatchinORM(object):
     @log_function_execution(logger=logger)
     def find_all(self, db: Session) -> List[Matching]:
         return db.query(Matching).all()
+
+    @log_function_execution(logger=logger)
+    def find_by_ids(self, image1_id: str, image2_id: str, db: Session) -> Matching:
+        matching = (
+            db.query(Matching)
+            .filter(Matching.image1_id == image1_id, Matching.image2_id == image2_id)
+            .first()
+        )
+        if matching:
+            return matching
+        else:
+            return (
+                db.query(Matching)
+                .filter(Matching.image2_id == image1_id, Matching.image1_id == image2_id)
+                .one()
+            )
+
+    @log_function_execution(logger=logger)
+    def create_one(self, diff_data: DiffCreateSchema, db: Session):
+        matching = Matching(**diff_data.model_dump())
+        db.add(matching)
+        return matching
