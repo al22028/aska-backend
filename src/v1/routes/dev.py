a2439337985fe3ee016e7aefec9f29634468f1e6
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-18 15:54:39 +0900

    :+1: add delete single user by id for dev endpoint #300

--- src/v1/routes/dev.py
+++ src/v1/routes/dev.py
@@ -2,9 +2,10 @@
 from aws_lambda_powertools import Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.event_handler.api_gateway import Router
-from aws_lambda_powertools.event_handler.openapi.params import Query
+from aws_lambda_powertools.event_handler.openapi.params import Path, Query
 from aws_lambda_powertools.shared.types import Annotated
 from controllers.dev import DevController
+from schemas.common import DeletedSchema
 from schemas.diff import DiffSchema
 
 app = APIGatewayRestResolver(debug=True)
@@ -29,3 +30,17 @@ def create_image_diff(
     ],
 ) -> DiffSchema:
     return controller.create_image_diff(image1_id=image1Id, image2_id=image2Id)
+
+
+@router.delete("/users/<userId>", tags=["Dev"], summary="ユーザーを削除")
+def delete_single_user_dev(
+    userId: Annotated[
+        str,
+        Path(
+            title="ユーザーID",
+            description="API側のユーザーのみ削除します。Cognitoのユーザーに関しては削除しません。",
+            example="e7b45a98-1031-7095-d7ee-6748af941d2a",
+        ),
+    ]
+) -> DeletedSchema:
+    return controller.delete_single_user(user_id=userId)
